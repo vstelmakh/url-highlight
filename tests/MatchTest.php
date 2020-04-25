ad520a1adf80937d5ab104bbcbb9a1260a9ff824
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2020-04-25 17:37:46 +0200

    add test for match

---
+++ tests/MatchTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace VStelmakh\UrlHighlight\Tests;
+
+use VStelmakh\UrlHighlight\Match;
+use PHPUnit\Framework\TestCase;
+
+class MatchTest extends TestCase
+{
+    public function testGetFullMatch(): void
+    {
+        $match = new Match('http://example.com', null, null, null, null);
+        $result = $match->getFullMatch();
+        $this->assertSame('http://example.com', $result);
+    }
+
+    public function testGetScheme(): void
+    {
+        $match = new Match('', 'http', null, null, null);
+        $result = $match->getScheme();
+        $this->assertSame('http', $result);
+    }
+
+    public function testGetLocal(): void
+    {
+        $match = new Match('', null, 'user:password', null, null);
+        $result = $match->getLocal();
+        $this->assertSame('user:password', $result);
+    }
+
+    public function testGetHost(): void
+    {
+        $match = new Match('', null, null, 'example.com', null);
+        $result = $match->getHost();
+        $this->assertSame('example.com', $result);
+    }
+
+    public function testGetTld(): void
+    {
+        $match = new Match('', null, null, null, 'com');
+        $result = $match->getTld();
+        $this->assertSame('com', $result);
+    }
+}
