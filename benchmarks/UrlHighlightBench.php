62878821eba374d3afa1b56e1969e0dab3d94fc0
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2020-08-20 15:00:42 +0200

    add highlight url encoded benchmarks

--- benchmarks/UrlHighlightBench.php
+++ benchmarks/UrlHighlightBench.php
@@ -4,6 +4,8 @@ namespace VStelmakh\UrlHighlight\Benchmarks;
 
 use PhpBench\Benchmark\Metadata\Annotations\Iterations;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
+use VStelmakh\UrlHighlight\Encoder\HtmlEntitiesEncoder;
+use VStelmakh\UrlHighlight\Encoder\HtmlSpecialcharsEncoder;
 use VStelmakh\UrlHighlight\UrlHighlight;
 
 class UrlHighlightBench
@@ -17,6 +19,30 @@ class UrlHighlightBench
         $urlHighlight = new UrlHighlight();
 
         $input = file_get_contents(__DIR__ . '/input_plain.txt');
+        $urlHighlight->highlightUrls($input);
+    }
+
+    /**
+     * @Revs(10)
+     * @Iterations(5)
+     */
+    public function benchHighlightHtmlSpecialChars(): void
+    {
+        $urlHighlight = new UrlHighlight(null, null, new HtmlSpecialcharsEncoder());
+
+        $input = file_get_contents(__DIR__ . '/input_html_special_chars.txt');
+        $urlHighlight->highlightUrls($input);
+    }
+
+    /**
+     * @Revs(10)
+     * @Iterations(5)
+     */
+    public function benchHighlightHtmlEntities(): void
+    {
+        $urlHighlight = new UrlHighlight(null, null, new HtmlEntitiesEncoder());
+
+        $input = file_get_contents(__DIR__ . '/input_html_entities.txt');
         $urlHighlight->highlightUrls($input);
     }
 }
