bbaab84dbce23f7c3b51eae9a392b759c77cdeea
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2026-07-30 11:53:40 +0200

    move domains list to matcher namespace

--- src/Matcher/Domains.php
+++ src/Matcher/Domains.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace VStelmakh\UrlHighlight;
+namespace VStelmakh\UrlHighlight\Matcher;
 
 /**
  * @internal
--- src/Matcher/HostValidator.php
+++ src/Matcher/HostValidator.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace VStelmakh\UrlHighlight\Matcher;
 
-use VStelmakh\UrlHighlight\Domains;
 use VStelmakh\UrlHighlight\Url;
 
 /**
